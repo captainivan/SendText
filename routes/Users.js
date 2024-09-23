525c10af8652c65757939892da01ac84f9aeb4d7
Frank Barlow <36685509-mirepeh15@users.noreply.replit.com>
2024-09-23 12:23:42 +0000

    Update Users.js and add vercel.json

--- routes/Users.js
+++ routes/Users.js
@@ -2,9 +2,7 @@ const express = require('express');
 const {sendText, receiveText} = require('../controllers/users');
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-  res.send({message:"hello world from route"});
-})
+
 
 router.post("/send",sendText)
 router.post("/receive",receiveText)
